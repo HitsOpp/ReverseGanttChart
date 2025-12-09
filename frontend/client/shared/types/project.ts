ddf962fe0d59cf01b1c8b15673e75d987a2893f0
denis <user@MacBook-Air-user-2.local>
2025-12-09 22:35:04 +0700

    acceptTasks

--- frontend/client/shared/types/project.ts
+++ frontend/client/shared/types/project.ts
@@ -9,10 +9,22 @@ export interface loadProjectType {
   createdAt: string;
 }
 
+type teamProgress = {
+  teamId: string;
+  teamName: string;
+  status: number;
+  completedDate: string;
+  completedByName: string;
+  completedStageCount: number;
+  totalStageCount: number;
+  progress: number;
+};
+
 export interface ProjectTaskType {
   id: string;
   name: string;
   description: string;
   dueDate?: string;
   priority?: number;
+  teamProgress: teamProgress[];
 }
